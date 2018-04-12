01d957f4de68c815adc21f31f77eeff36f47bba7
prajakta <prajakta.kamble@wohlig.com>
2018-04-12 19:08:33 +0530

    login

--- www/js/loginctrl.js
+++ www/js/loginctrl.js
@@ -2,63 +2,63 @@ myApp.controller("LoginCtrl", function ($scope, Service, $state, $ionicPlatform,
   $ionicPlatform.ready(function () {
     screen.orientation.lock('portrait');
   })
-  screen.orientation.lock('portrait');
-  $ionicModal.fromTemplateUrl('templates/modal/message.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function (modal) {
-    $scope.messageModal = modal;
-  });
+  // screen.orientation.lock('portrait');
+  // $ionicModal.fromTemplateUrl('templates/modal/message.html', {
+  //   scope: $scope,
+  //   animation: 'slide-in-up'
+  // }).then(function (modal) {
+  //   $scope.messageModal = modal;
+  // });
 
-  $scope.showMessageModal = function () {
-    $scope.messageModal.show();
-    $timeout(function () {
-      $scope.closeMessageModal();
-    }, 2000);
-  };
-  $scope.closeMessageModal = function () {
-    $scope.messageModal.hide();
-  };
+  // $scope.showMessageModal = function () {
+  //   $scope.messageModal.show();
+  //   $timeout(function () {
+  //     $scope.closeMessageModal();
+  //   }, 2000);
+  // };
+  // $scope.closeMessageModal = function () {
+  //   $scope.messageModal.hide();
+  // };
 
-  $scope.invalidUser = false;
-  $scope.playerLogin = function (data, login) {
-    $scope.loginPromise = Service.playerLogin(data, function (data) {
-      $.jStorage.set("accessToken", data.data);
-      if (data && !_.isEmpty(data.data)) {
-        $state.go("lobby");
-      } else if (data.error == "Member already Logged In") {
-        $scope.message = {
-          heading: "User Already Loged In",
-          content: "User already loged in another device. Logout from that device. Try Again!!!"
-        };
-        $scope.showMessageModal();
-      } else if(data.error=="Login denied")
-      {
-        $scope.message = {
-          heading: "Login denied",
-          content: "Login denied"
-        };
-        $scope.showMessageModal();
-      }
-        
-        else {
-        $scope.message = {
-          heading: "Incorrect Username Password",
-          content: "Try Again!!!"
-        };
-        $scope.showMessageModal();
-      }
-    });
-  };
+  // $scope.invalidUser = false;
+  // $scope.playerLogin = function (data, login) {
+  //   $scope.loginPromise = Service.playerLogin(data, function (data) {
+  //     $.jStorage.set("accessToken", data.data);
+  //     if (data && !_.isEmpty(data.data)) {
+  //       $state.go("lobby");
+  //     } else if (data.error == "Member already Logged In") {
+  //       $scope.message = {
+  //         heading: "User Already Loged In",
+  //         content: "User already loged in another device. Logout from that device. Try Again!!!"
+  //       };
+  //       $scope.showMessageModal();
+  //     } else if(data.error=="Login denied")
+  //     {
+  //       $scope.message = {
+  //         heading: "Login denied",
+  //         content: "Login denied"
+  //       };
+  //       $scope.showMessageModal();
+  //     }
+
+  //       else {
+  //       $scope.message = {
+  //         heading: "Incorrect Username Password",
+  //         content: "Try Again!!!"
+  //       };
+  //       $scope.showMessageModal();
+  //     }
+  //   });
+  // };
 
 
-  //js Storage 
-  $scope.accessToken = $.jStorage.get("accessToken");
-  if ($scope.accessToken) {
-    $state.go("lobby");
-  }
-  $ionicPlatform.registerBackButtonAction(function (event) {
-    ionic.Platform.exitApp();
-    // event.preventDefault();
-  }, 100);
+  // //js Storage 
+  // $scope.accessToken = $.jStorage.get("accessToken");
+  // if ($scope.accessToken) {
+  //   $state.go("lobby");
+  // }
+  // $ionicPlatform.registerBackButtonAction(function (event) {
+  //   ionic.Platform.exitApp();
+  //   // event.preventDefault();
+  // }, 100);
 });
