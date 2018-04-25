415eb7c084851451bdf49ed8f1840fa264499b17
maheshnadar <mahesh.nadar@wohlig.com>
2018-04-25 10:15:28 +0530

    table

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var myApp = angular.module('starter', ['ionic', 'starter.service', 'ui.select', 'ngSanitize', 'angularPromiseButtons', 'ui-rangeSlider'])
+var myApp = angular.module('starter', ['ionic', 'starter.service', 'ui.select', 'ngSanitize', 'angularPromiseButtons', 'rzModule'])
 
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
