179ec8d96510087fd412e01cc656abc61cb1b2f6
sohan1 <shn619@gmail.com>
2018-11-19 11:22:37 +0530

    table

--- www/js/app.js
+++ www/js/app.js
@@ -4,10 +4,19 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var myApp = angular.module('starter', ['ionic', 'starter.service', 'ui.select', 'ngSanitize', 'angularPromiseButtons', 'rzModule', 'ngAnimate'])
+var myApp = angular
+  .module("starter", [
+    "ionic",
+    "starter.service",
+    "ui.select",
+    "ngSanitize",
+    "angularPromiseButtons",
+    "rzModule",
+    "ngAnimate"
+  ])
 
-  .run(function ($ionicPlatform, $state, $timeout) {
-    $ionicPlatform.ready(function () {
+  .run(function($ionicPlatform, $state, $timeout) {
+    $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.MobileAccessibility) {
@@ -31,20 +40,26 @@ var myApp = angular.module('starter', ['ionic', 'starter.service', 'ui.select', 
         StatusBar.hide();
       }
 
-
-
       if (window.plugins && window.plugins.insomnia) {
         window.plugins.insomnia.keepAwake();
       }
 
       // Preload audio resources
-      $ionicPlatform.ready(function () {
+      $ionicPlatform.ready(function() {
         if (window.cordova) {
-          window.plugins.NativeAudio.preloadComplex('turn', 'audio/win.mp3', 1, 1, 0, function (msg) {}, function (msg) {
-            console.log('error: ' + msg);
-          });
+          window.plugins.NativeAudio.preloadComplex(
+            "turn",
+            "audio/win.mp3",
+            1,
+            1,
+            0,
+            function(msg) {},
+            function(msg) {
+              console.log("error: " + msg);
+            }
+          );
         }
-      })
+      });
       // window.plugins.NativeAudio.preloadComplex('coin', 'audio/coin.mp3', 1, 1, 0, function (msg) {}, function (msg) {
       //   console.log('error: ' + msg);
       // });
@@ -57,15 +72,24 @@ var myApp = angular.module('starter', ['ionic', 'starter.service', 'ui.select', 
       // window.plugins.NativeAudio.preloadComplex('button', 'audio/button.mp3', 1, 1, 0, function (msg) {}, function (msg) {
       //   console.log('error: ' + msg);
       // });
-
     });
   })
   // .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
 
   //   }]
-  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  .config(function(
+    $stateProvider,
+    $urlRouterProvider,
+    $ionicConfigProvider,
+    $sceDelegateProvider
+  ) {
     $ionicConfigProvider.views.maxCache(10);
     $ionicConfigProvider.views.swipeBackEnabled(false);
+    $sceDelegateProvider.resourceUrlWhitelist([
+      "self",
+      new RegExp("^(http[s]?)://(w{3}.)?youtube.com/.+$")
+    ]);
+
     //for loader
     // cfpLoadingBarProvider.includeSpinner = true;
     // cfpLoadingBarProvider.includeBar = false;
@@ -79,52 +103,48 @@ var myApp = angular.module('starter', ['ionic', 'starter.service', 'ui.select', 
       //   controller: 'AppCtrl'
       // })
 
-      .state('lobby', {
-        url: '/lobby',
+      .state("lobby", {
+        url: "/lobby",
         cache: false,
-        templateUrl: 'templates/lobby.html',
-        controller: 'LobbyCtrl'
+        templateUrl: "templates/lobby.html",
+        controller: "LobbyCtrl"
       })
-      .state('login', {
-        url: '/login',
+      .state("login", {
+        url: "/login",
         cache: false,
-        templateUrl: 'templates/login.html',
-        controller: 'LoginCtrl'
+        templateUrl: "templates/login.html",
+        controller: "LoginCtrl"
       })
-      .state('signup', {
-        url: '/signup',
-        templateUrl: 'templates/sign-up.html',
+      .state("signup", {
+        url: "/signup",
+        templateUrl: "templates/sign-up.html",
         cache: false,
-        controller: 'SignUpCtrl'
+        controller: "SignUpCtrl"
       })
-      .state('addamount', {
-        url: '/addamount',
-        templateUrl: 'templates/add-amount.html',
+      .state("addamount", {
+        url: "/addamount",
+        templateUrl: "templates/add-amount.html",
         cache: false,
-        controller: 'AddAmountCtrl'
+        controller: "AddAmountCtrl"
       })
-      .state('withdrawal', {
-        url: '/withdrawal',
-        templateUrl: 'templates/withdrawal-amount.html',
+      .state("withdrawal", {
+        url: "/withdrawal",
+        templateUrl: "templates/withdrawal-amount.html",
         cache: false,
-        controller: 'WithDrawalCtrl'
+        controller: "WithDrawalCtrl"
       })
-      .state('table', {
-        url: '/table/:id',
+      .state("table", {
+        url: "/table/:id",
         cache: false,
-        templateUrl: 'templates/table.html',
-        controller: 'TableCtrl'
-      })
-
-    ;
+        templateUrl: "templates/table.html",
+        controller: "TableCtrl"
+      });
     // if none of the above states are matche d, use this as the fallback
-    $urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise("/login");
   });
 
-
-
-myApp.filter('uploadpath', function () {
-  return function (input, width, height, style) {
+myApp.filter("uploadpath", function() {
+  return function(input, width, height, style) {
     var other = "";
     if (width && width !== "") {
       other += "&width=" + width;
@@ -136,7 +156,7 @@ myApp.filter('uploadpath', function () {
       other += "&style=" + style;
     }
     if (input) {
-      if (input.indexOf('https://') == -1) {
+      if (input.indexOf("https://") == -1) {
         return imgpath + "?file=" + input + other;
       } else {
         return input;
@@ -145,10 +165,9 @@ myApp.filter('uploadpath', function () {
   };
 });
 
-myApp.filter('serverimage', function () {
-  return function (input, width, height, style) {
+myApp.filter("serverimage", function() {
+  return function(input, width, height, style) {
     if (input) {
-
       if (input.substr(0, 4) == "http") {
         return input;
       } else {
@@ -164,7 +183,6 @@ myApp.filter('serverimage', function () {
         }
         return image;
       }
-
     } else {
       //    return "img/logo.png";
       return "img/player_pic.png";
@@ -172,11 +190,10 @@ myApp.filter('serverimage', function () {
   };
 });
 
-myApp.filter('cardimg', function () {
-  return function (input) {
+myApp.filter("cardimg", function() {
+  return function(input) {
     if (input) {
-      return "img/cards/" + input + ".svg"
-
+      return "img/cards/" + input + ".svg";
     } else {
       //    return "img/logo.png";
       return "img/player_pic.png";
@@ -184,8 +201,8 @@ myApp.filter('cardimg', function () {
   };
 });
 
-myApp.filter('trustUrl', function ($sce) {
-  return function (url) {
+myApp.filter("trustUrl", function($sce) {
+  return function(url) {
     return $sce.trustAsResourceUrl(url);
   };
 });
